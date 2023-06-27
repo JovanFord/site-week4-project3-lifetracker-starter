fb606fefe34cb4eb077a788580431ef0b4aec293
Jovan Ford <jovan.ford@jovanfo-ltm9wmy.internal.salesforce.com>
2023-06-27 15:13:36 -0700

    added jsx files

---
+++ lifetracker-ui/src/components/FeedTiles/FeedTiles.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FeedTiles = () => {
+  return (
+    <div>FeedTiles</div>
+  )
+}
+
+export default FeedTiles
---
+++ lifetracker-ui/src/components/Home/Hame.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Hame = () => {
+  return (
+    <div>Hame</div>
+  )
+}
+
+export default Hame
